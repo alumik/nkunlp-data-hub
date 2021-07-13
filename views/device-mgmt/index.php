42b302711e125240b2585e1f518b8483a83391b1
Zhong Zhenyu <nczzy1997@gmail.com>
2021-07-14 02:25:17 +0800

    Fix: extra view buttons

--- views/device-mgmt/index.php
+++ views/device-mgmt/index.php
@@ -32,7 +32,10 @@ $this->params['breadcrumbs'][] = $this->title;
             'notes:ntext',
             'updated_at',
 
-            ['class' => 'yii\grid\ActionColumn'],
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'template' => '{update} {delete}',
+            ],
         ],
     ]); ?>
 
